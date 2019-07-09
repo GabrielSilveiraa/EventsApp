e127c68faee90105d24b2f76cf89ad525093b33d
Gabriel Silveira <gabi.msilveira@gmail.com>
2019-07-09 20:37:21 -0300

    Changed files path and removed appDelegates's unused functions

---
+++ EventsApp/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  EventsApp
+//
+//  Created by Gabriel Silveira on 09/07/19.
+//  Copyright © 2019 Gabriel Silveira. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
